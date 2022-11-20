4fce4d85dcb54f6ff35a60430f7ae79be13ff4f9
HyungJin <hyungjin0710@gmail.com>
2022-11-20 23:50:28 +0900

    Add 11660_구간 합 구하기 5.py

---
+++ BAEKJOON/Dynamic Programming/11660_구간 합 구하기 5.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = [[0] * (N + 1)]
+
+for _ in range(N):
+    xy = [0] + [int(x) for x in input().split()]
+    arr.append(xy)
+
+for i in range(1, N + 1):
+    for j in range(1, N):
+        arr[i][j + 1] += arr[i][j]
+
+for j in range(1, N + 1):
+    for i in range(1, N):
+        arr[i + 1][j] += arr[i][j]
+        
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1])
