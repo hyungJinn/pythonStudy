c87eabba7fdf20c7ce765d11acc5208007ed0f14
HyungJin <hyungjin0710@gmail.com>
2022-12-12 17:55:46 +0900

    Add 1620_나는야 포켓몬 마스터 이다솜.py

---
+++ BAEKJOON/Data Structures/1620_나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,21 @@
+# https://gudwns1243.tistory.com/63
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+dict = {}
+
+for i in range(1, N + 1):
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
+
+for i in range(M):
+    quest = input().rstrip()
+    if quest.isdigit():
+        print(dict[int(quest)])
+    else:
+        print(dict[quest])
+    
