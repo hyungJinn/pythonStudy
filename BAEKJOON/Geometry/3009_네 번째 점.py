a9ae580c22174b2d0af8755e493a2c85f8e9b50c
HyungJin <hyungjin0710@gmail.com>
2022-12-14 00:04:19 +0900

    Add 3009_네 번째 점.py

---
+++ BAEKJOON/Geometry/3009_네 번째 점.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+xs = []
+ys = []
+for i in range(3):
+    x, y = map(int, input().split())
+    xs.append(x)
+    ys.append(y)
+
+for i in range(3):
+    if xs.count(xs[i]) == 1:
+        x4 = xs[i]
+    if ys.count(ys[i]) == 1:
+        y4 = ys[i]
+
+print(x4, y4)
