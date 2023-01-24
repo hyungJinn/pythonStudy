2c0d92c2a68cbe5ea58992b65cbe130f0e53c009
hyungJinn <hyungjinnh@gmail.com>
2023-01-25 01:42:13 +0900

    Update 17298.py

--- BAEKJOON/Data Structures/17298.py
+++ BAEKJOON/Data Structures/17298.py
@@ -2,27 +2,32 @@
 # https://hooongs.tistory.com/329
 # Data Structures - Stack
 
+# 스택은 값이 아닌 '인덱스'를 저장해야 한다!!!
+
 import sys
 input = sys.stdin.readline
 from collections import deque
 
 N = int(input())
-seq = list(map(int, input().split()))
+seq = list(map(int, input().split())) # sequence = 수열
 
-NGEs = [-1] * N # 오큰수, [-1, -1, ..., -1]
-S = deque() # stack
+# 오큰수, [-1, -1, ..., -1]
+# 오른수가 없을 때 -1을 출력하기 때문에, 오큰수 리스트를 -1로 초기화
+# Ai에 대한 오큰수 = NGEs(Ai) = 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수
+NGEs = [-1] * N 
+s = deque() # stack
 
 for i in range(N):
-    while S and (S[-1][0] < seq[i]):
-        tmp, idx = S.pop()
+    while s and (s[-1][0] < seq[i]):
+        tmp, idx = s.pop()
         NGEs[idx] = seq[i]
-    S.append([seq[i], i])
+    s.append([seq[i], i])
 
 print(*NGEs)
 
 # O(N^2)이 걸릴수도 있는 문제를 스택을 사용해 O(N)에 가깝게 만드는 문제.
-
-# Ai에 대한 오큰수 = NGEs(Ai) = 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수
+# 스택에 원소를 넣을 때 해당 값의 인덱스를 함께 넣어주어야,
+# 스택에서 pop연산을 하고 오큰수를 업데이트하는데에서 시간을 줄일 수 있다.
 
 # 왼쪽에서부터 값을 스택에 넣으면서 알고리즘을 진행해나간다.
 # 스택에 가장 위에 있는 값이 현재의 값보다 작거나 스택이 빌때까지 pop연산을 해준다.
