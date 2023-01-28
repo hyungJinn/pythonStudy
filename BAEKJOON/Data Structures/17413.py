109413cd2ca073664d024e741f6c9e03b578ff39
hyungJinn <hyungjinnh@gmail.com>
2023-01-28 19:11:50 +0900

    Update 17413.py

--- BAEKJOON/Data Structures/17413.py
+++ BAEKJOON/Data Structures/17413.py
@@ -1,34 +1,38 @@
 # 17413_단어 뒤집기 2.py
+# https://star7sss.tistory.com/391
 # Data Structures - Stack
 
-str = list(input())
+str = list(input()) # 뒤에 공백 문자 없다. ['a', 'b', 'c']
 stack = []
 result = ''
 flag = False
 
 for s in str:
-    if flag == False:
-        if s == '<':
-            flag = True
-            while stack:
-                result += stack.pop()
-            stack.append(s)
-        elif s == ' ':
-            while stack:
-                result += stack.pop()
-            result += s
-        else:
-            stack.append(s)
-            
-    elif flag == True:
+    # 공백이면 stack을 비움
+    if s == ' ':
+        while stack:
+            result += stack.pop()
+        result += s
+    
+    # 태그 안 단어는 뒤집지 않음
+    elif s == '<':
+        while stack:
+            result += stack.pop()
+        flag = True
+        result += s
+    
+    elif s == '>':
+        flag = False
+        result += s
+    
+    elif flag:
+        result += s
+    
+    # 태그 밖 단어는 뒤집음
+    else:
         stack.append(s)
-        if s == '>':
-            flag = False
-            stack.reverse()
-            while stack:
-                result += stack.pop()
-
+        
+# 출력
 while stack:
     result += stack.pop()
-
 print(result)
